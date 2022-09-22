7341f7500e9c36afc9072460e2d9caeea1b0b1a2
Eduardo <edu.fss14@gmail.com>
2022-09-22 19:57:49 -0300

    first commit

---
+++ contato.php
@@ -0,0 +1,22 @@
+<?php
+include "header.php";
+?>
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <h1>Fale conosco</h1>
+      <div class="">
+        <form action="">
+          <label for="nome"></label>
+          <input type="text" name="nome" id="nome">
+          <label for="email">Digite seu e-mail</label>
+          <input type="text" name="email" id="email">
+          <input type="submit" value="Enviar">
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+include "footer.php";
+?>
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    fausto: "./src/js/index.js",
+  },
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "assets"),
+    filename: "js/[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            "presets": [
+              ["@babel/preset-env", {
+                "useBuiltIns": "usage",
+                "corejs": {
+                  "version": 3,
+                  "proposals": true
+                }
+              }]
+            ]
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+  ],
+};
