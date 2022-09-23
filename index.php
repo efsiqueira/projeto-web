d7a042efd66e07603f672cbe0e0f615ffd4cf1c0
Eduardo <edu.fss14@gmail.com>
2022-09-22 21:13:07 -0300

    corrigindo identação

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 include "header.php";
 ?>
 <div class="container">
-    <div class="row">
-        <div class="col-6"><img src="assets/images/download.jpg" alt="random image"></div>
-        <div class="col-6">
-            <a href="//google.com" target="_blank" class="botao">Visite o Google</a>
-        </div>
-    </div>
+	<div class="row">
+		<div class="col-6"><img src="assets/images/download.jpg" alt="random image"></div>
+		<div class="col-6">
+			<a href="//google.com" target="_blank" class="botao">Visite o Google</a>
+		</div>
+	</div>
 </div>
 <?php
 include "footer.php";
--- nav.php
+++ nav.php
@@ -1,10 +1,10 @@
 
 <nav class="main">
-    <ul calss="container">
-        <li><img src="//picsum.photos/100/60"></li>
-        <li><a href="index.php">INICIO</a></li>
-        <li><a href="sobre.php">SOBRE</a></li>
-        <li><a href="contato.php">CONTATO</a></li>
-        <li><a href="sac.php">SAC</a></li>
-    </ul>
+	<ul calss="container">
+		<li><img src="//picsum.photos/100/60"></li>
+		<li><a href="index.php">INICIO</a></li>
+		<li><a href="sobre.php">SOBRE</a></li>
+		<li><a href="contato.php">CONTATO</a></li>
+		<li><a href="sac.php">SAC</a></li>
+	</ul>
 </nav>
