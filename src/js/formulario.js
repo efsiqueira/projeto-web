0f9a275de7a3fdd55a19e6e4790c6a79b9df4962
Eduardo <edu.fss14@gmail.com>
2022-09-22 22:10:32 -0300

    ajustando função do formulário

--- src/js/formulario.js
+++ src/js/formulario.js
@@ -2,17 +2,20 @@ const initForm = (qualForm) => {
   const formRequerido = qualForm
   const inputs = formRequerido.querySelectorAll("input")
 
-  if(inputs.length > 0) {
+  if (inputs) {
     inputs.forEach((input) => {
       console.log(input.value)
     })
+  } else {
+    console.log("Não existem inputs neste formulário")
   }
 }
 
 const init = (qualForm) => {
   const formRequerido = document.getElementById(qualForm)
-  const submit = formRequerido.querySelector("input[type=submit")
+  const submit = formRequerido.querySelector("input[type=submit]")
   submit.addEventListener("click", (evento) => {
+    evento.preventDefault()
     initForm(formRequerido)
   })
 }
